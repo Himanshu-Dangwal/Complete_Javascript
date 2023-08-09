a4d079b882b3f5b036bedc75c9eea54f17e10c98
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-08-09 13:16:12 +0530

    use strict mode

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Use below code first with strict mode and then without strict modex
+// let values = 20;
+// if(true) value = 30;
+// console.log(values);
+
+
+// let a = 10;
+
+// let val = a>=5 ? a+=2 : a-=2;
+
+// console.log(a,val);
+
