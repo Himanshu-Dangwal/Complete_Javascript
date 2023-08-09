045041974bd0905387db8531981d93ccd28501d0
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-08-09 14:26:34 +0530

    Assignment functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -12,3 +12,23 @@
 
 // console.log(a,val);
 
+/* Write your code below. Good luck! 🙂 */
+
+const calcAverage = (a,b,c) => {
+    return (a+b+c)/3;
+}
+
+let scoreDolphins = calcAverage(44,23,71);
+let scoreKoalas  = calcAverage(23,34,27);
+
+function checkWinner(avgDolphins,avgKoalas){
+    if(avgDolphins > 2*avgKoalas){
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas}`)
+    }else if(avgKoalas > 2*avgDolphins){
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins}`)
+    }else{
+        console.log("No team wins...")
+    }
+}
+
+checkWinner(scoreDolphins,scoreKoalas);
